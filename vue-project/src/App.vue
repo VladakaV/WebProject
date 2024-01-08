f3c41bc5f19fa9cc876d46b4b0cbbf2ea2361903
24broken24 <ienvadayr@mail.ru>
2024-01-08 15:22:39 +0300

    footer

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -8,7 +8,7 @@
   <Team/>
   <Cases/>
   <FAQ/>
-
+  <Footer1/>
 </template>
 
 <script>
@@ -23,8 +23,7 @@ import Team from "../Components/Team";
 import Cases from "../Components/Cases";
 import Partners from "../Components/Partners";
 import FAQ from "../Components/FAQ";
-
-
+import Footer1 from "../Components/Footer1";
 
 export default {
   components: {
@@ -38,6 +37,7 @@ export default {
     Cases,
     Partners,
     FAQ,
+    Footer1,
   }
 }
 </script>
